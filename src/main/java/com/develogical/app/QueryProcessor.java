9af5d16de78a0f942ddd80e45e18979bc7d798be
adeel741 <msc_finance@yahoo.com>
2016-07-14 15:34:04 +0100

    QueryProcess class modified

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -1,17 +1,28 @@
 package com.develogical.app;
 
+import com.sun.tools.javac.util.ArrayUtils;
+import org.eclipse.jetty.util.ArrayUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-                    "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.toLowerCase().contains("which of the following numbers is the largest: 2090, 20278, 20192"))
-        {
-            return "20278";
+        int max = 0;
+        String[] queryArray = null;
+        String[] queryNumbers = null;
+        int[] queryInt = null;
+        queryNumbers = query.split(":")[1].split(",");
+        for (int i=0;i<queryNumbers.length;i++){
+            queryInt[i] = Integer.parseInt(queryNumbers[i]);
+            if (queryInt[i] > max){max = queryInt[i];}
         }
 
+        return ""+max;
+
+
 //        else if (query.toLowerCase().contains("what is your name"))
 //        {
 //            return "PIP Team";
@@ -27,6 +38,5 @@ public class QueryProcessor {
 //            return "PIP Team";
 //        }
 
-        return "";
     }
 }
