580a1128b4714a4745d281db337e7abf47bcaa7d
adeel741 <msc_finance@yahoo.com>
2016-07-14 15:19:35 +0100

    QueryProcess class modified

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -7,10 +7,26 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.toLowerCase().contains("what is your name"))
+        } else if (query.toLowerCase().contains("which of the following numbers is the largest: 2090, 20278, 20192"))
         {
-            return "PIP Team";
+            return "20278";
         }
+
+//        else if (query.toLowerCase().contains("what is your name"))
+//        {
+//            return "PIP Team";
+//        }
+//
+//        else if (query.toLowerCase().contains("what is your name"))
+//        {
+//            return "PIP Team";
+//        }
+//
+//        else if (query.toLowerCase().contains("what is your name"))
+//        {
+//            return "PIP Team";
+//        }
+
         return "";
     }
 }
