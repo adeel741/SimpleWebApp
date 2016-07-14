ab138026c68d253d6239a0709204a6af21001dea
adeel741 <msc_finance@yahoo.com>
2016-07-14 15:39:09 +0100

    QueryProcess class modified

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -1,20 +1,14 @@
 package com.develogical.app;
 
-import com.sun.tools.javac.util.ArrayUtils;
-import org.eclipse.jetty.util.ArrayUtil;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 
 public class QueryProcessor {
 
     public String process(String query) {
         int max = 0;
-        String[] queryArray = null;
         String[] queryNumbers = null;
         int[] queryInt = null;
-        queryNumbers = query.split(":")[1].split(",");
+        String[] queryArray = query.split(":");
+        queryNumbers = queryArray[1].split(",");
         for (int i=0;i<queryNumbers.length;i++){
             queryInt[i] = Integer.parseInt(queryNumbers[i]);
             if (queryInt[i] > max){max = queryInt[i];}
